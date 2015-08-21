dbe9d0a648ddb6ddb303dca283599364695d4372
Grace <grace_lie91@yahoo.com>
2015-08-21 16:35:37 +0700

    commit

---
+++ AndroidLoginAndRegistration/src/com/parking/activity/RegisterActivity.java
@@ -0,0 +1,98 @@
+/**
+ * Author: Ravi Tamada
+ * URL: www.androidhive.info
+ * twitter: http://twitter.com/ravitamada
+ * */
+package com.parking.activity;
+
+import info.androidhive.loginandregistration.R;
+import info.androidhive.loginandregistration.app.AppConfig;
+import info.androidhive.loginandregistration.app.AppController;
+import info.androidhive.loginandregistration.helper.SQLiteHandler;
+import info.androidhive.loginandregistration.helper.SessionManager;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import android.R.string;
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.android.volley.Request.Method;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+
+public class RegisterActivity extends Activity {
+	private static final String TAG = RegisterActivity.class.getSimpleName();
+	private Button btnRegister;
+	private Button btnLinkToLogin;
+	private EditText inputFullName;
+	private EditText inputEmail;
+	private EditText inputPassword;
+	private EditText inputLicenseNo;
+	private EditText inputPhoneNo;
+	private ProgressDialog pDialog;
+	private SessionManager session;
+	private SQLiteHandler db;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_register);
+
+		inputFullName = (EditText) findViewById(R.id.name);
+		inputEmail = (EditText) findViewById(R.id.email);
+		inputLicenseNo = (EditText) findViewById(R.id.licenseNo);
+		inputPhoneNo = (EditText) findViewById(R.id.phoneNo);
+		inputPassword = (EditText) findViewById(R.id.password);
+		btnRegister = (Button) findViewById(R.id.btnRegister);
+		btnLinkToLogin = (Button) findViewById(R.id.btnLinkToLoginScreen);
+
+		// Progress dialog
+		pDialog = new ProgressDialog(this);
+		pDialog.setCancelable(false);
+
+		
+
+		// Register Button Click event
+		btnRegister.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View view) {
+				String name = inputFullName.getText().toString();
+				String email = inputEmail.getText().toString();
+				String password = inputPassword.getText().toString();
+				String phoneNo = inputPhoneNo.getText().toString();
+				String licenseNo = inputLicenseNo.getText().toString();
+
+				if (!name.isEmpty() && !email.isEmpty() && !password.isEmpty()&& !phoneNo.isEmpty()&& !licenseNo.isEmpty()) {
+					
+				} else {
+					Toast.makeText(getApplicationContext(),
+							"Please fill your details!", Toast.LENGTH_LONG).show();
+				}
+			}
+		});
+
+		// Link to Login Screen
+		btnLinkToLogin.setOnClickListener(new View.OnClickListener() {
+
+			public void onClick(View view) {
+				Intent i = new Intent(getApplicationContext(), LoginActivity.class);
+				startActivity(i);
+				finish();
+			}
+		});
+
+	}
+
+}
