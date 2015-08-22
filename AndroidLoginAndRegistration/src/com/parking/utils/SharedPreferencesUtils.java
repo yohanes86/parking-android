0377e176f9a3603e4e08c027309058544dc8808b
yohanes <vincentius.yohanes86@gmail.com>
2015-08-22 17:06:14 +0700

    aaa

--- AndroidLoginAndRegistration/src/com/parking/utils/SharedPreferencesUtils.java
+++ AndroidLoginAndRegistration/src/com/parking/utils/SharedPreferencesUtils.java
@@ -2,7 +2,7 @@ package com.parking.utils;
 
 import android.content.SharedPreferences;
 
-public class StringUtils {
+public class SharedPreferencesUtils {
 	private static SharedPreferences prefs;
 	private static String prefName = "SharePrefLogin";
 }
